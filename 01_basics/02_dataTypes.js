c8e9739d0ac7580e89f28215ef5d05b8879d291a
Vaibhav-09-P <134619542+Vaibhav-09-P@users.noreply.github.com>
2024-07-14 06:51:50 +0000

    added datatypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer version
+// alert("Hello") // We are using node.js, not browser
+
+// console.log(3+3); console.log("Vaibhav")
+
+let name = "Vaibhav"
+let age = "20"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Vaibhav");
+console.log(typeof undefined); // undefined type is undefined
+console.log(typeof null); // null type is object
